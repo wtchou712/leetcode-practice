a33bed6e4ab5c6d8f0a6d9e36d3cc14d885e1f2e
William Chou <wtchou712@gmail.com>
2018-02-19 22:25:14 -0800

    pascals triangle

--- DP.java
+++ DP.java
@@ -33,3 +33,34 @@ public List<List<Integer>> generate(int numRows) {
     }
     return pascal;
 }
+
+/* Pascal's Triangle II
+
+Given an index k, return the kth row of the Pascal's triangle.
+
+For example, given k = 3,
+Return [1,3,3,1].
+
+Note:
+Could you optimize your algorithm to use only O(k) extra space?
+
+https://leetcode.com/problems/pascals-triangle-ii/description/
+
+*/
+public List<Integer> getRow(int rowIndex) {
+    //1
+    //11
+    //121
+    //1331
+    //14641
+    //dynamic programming, builds array from end
+    //solution works by adding current number above and above left.
+    ArrayList<Integer> pascal = new ArrayList<Integer>();
+    pascal.add(1);//initialize first row
+    for(int row=1; row<rowIndex+1; row++){
+        pascal.add(0);//add 0 to increase size of array
+        for(int i=row; i>=1; i--)
+            pascal.set(i, pascal.get(i-1)+pascal.get(i));
+    }
+    return pascal;
+}
